e3f2f8a4c63c1cbfce89ce3a7bd09a5398d7400b
Eduardo Quintero Juan <eduardoquiju@hotmail.com>
2019-06-16 19:58:24 -0500

    add scene and textures

--- Demo Experiencia 3D/ViewController.swift
+++ Demo Experiencia 3D/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var viewScene: SCNView!
     var myScene = SCNScene(named: "art.scnassets/Mundo.scn")
-    //let video = AVPlayer(url: URL(fileURLWithPath: (Bundle.main.path(forResource: "Twin Star Exorcist - Opening 4  Kanadeai", ofType: ".mp4") ?? nil)!))
+    //let video = AVPlayer(url: URL(fileURLWithPath: ((Bundle.main.path(forResource: "Twin Star Exorcist - Opening 4  Kanadeai", ofType: ".mp4") ?? nil)!)))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
         scnView.scene = myScene
         
         scnView.showsStatistics = true
+        scnView.allowsCameraControl = true
         scnView.backgroundColor = UIColor.red
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
@@ -41,11 +42,49 @@ class ViewController: UIViewController {
         
         // check what nodes are tapped
         // --- Posicion de pantalla
-        let p = gestureRecognize.location(in: scnView)
-        //print(p)
+        let pointView = gestureRecognize.location(in: scnView)
         // ---
-        let hitResults = scnView.hitTest(p, options: [:])
+        let hitResults = scnView.hitTest(pointView, options: [:])
         print(hitResults)
+        
+        if hitResults.count > 0 {
+            // retrieved the first clicked object
+            let result = hitResults[0]
+            SCNTransaction.animationDuration = 0.05
+            
+            if(result.node.name == "box12"){
+                //result.node.position = SCNVector3(0, 2, 0)
+                //print(result.node.childNodes)
+                print(result.node.parent)
+                var selec = result.node.parent
+                
+                selec?.rotation = SCNVector4(-45, 0, 0, 1)
+                //let base = myScene?.rootNode.childNode
+                //base?.rotation = SCNVector4(-90, 0, 0, 1)
+                
+            }
+            
+            // get its material
+            let material = result.node.geometry!.firstMaterial!
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = 0.5
+            
+            // on completion - unhighlight
+            SCNTransaction.completionBlock = {
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5
+                
+                material.emission.contents = UIColor.black
+                
+                SCNTransaction.commit()
+            }
+            
+            material.emission.contents = UIColor.blue
+            
+            SCNTransaction.commit()
+        }
    
     }
 
